ffbc21be9c3af3123a56328b4337556b965b2cd4
Force Developments <79415118+Force-Developing@users.noreply.github.com>
2023-09-01 10:15:06 +0200

    Add files via upload

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ games { 'gta5' }
 fivem_checker 'yes'
 github 'https://github.com/Force-Developing/force_lib'
 name '^2force_lib'
-version '1.0'
+version '1.1'
 
 author 'Force#3883'
 description 'force_lib this is mainly for functions that i use in my scripts so i dont need to make unnecessary functions in every script'
