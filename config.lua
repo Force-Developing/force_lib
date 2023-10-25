5ff12643736c9cf3b3c9dfb9420167c2bfadde25
Force Developments <79415118+Force-Developing@users.noreply.github.com>
2023-10-25 09:18:53 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -6,6 +6,7 @@ Config = {
     ESX = true,
     QBCORE = false,
 
+       -- [**ESX**]
     SQL = {
         Tables = {
             character = "users",
@@ -27,4 +28,27 @@ Config = {
             jobGradesGrade= "grade",
         }
     }
+
+    -- [**QBCORE**]
+    -- SQL = {
+    --     Tables = {
+    --         character = "players",
+    --         vehicles = "garages",
+    --         licenses = "user_licenses",
+    --         jobs = "jobs",
+    --         jobGrades = "job_grades",
+    --     },
+
+    --     Columns = {
+    --         identifier = "citizenid",
+    --         owner = "owner", -- This is for example owned_vehicles identifier
+    --         garage = "garage", -- This is for example owned_vehicles in what garage it is
+    --         plate = "plate", -- This is for example owned_vehicles that the plate is stored
+    --         vehicleinfo = "vehicleinfo", -- This is for example owned_vehicles vehicleInfo
+    --         licensesOwner = "owner", -- This is for example user_licenses owner where the identifier for the characters license is
+    --         jobName = "name",
+    --         jobGradesName = "job_name",
+    --         jobGradesGrade= "grade",
+    --     }
+    -- }
 }
