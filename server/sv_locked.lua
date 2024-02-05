1ef419e1a8b56fe27b2811398a05e4829f62d86c
Force Developments <79415118+Force-Developing@users.noreply.github.com>
2024-02-05 14:37:38 +0100

    Delete server/sv_locked.lua

--- server/sv_locked.lua
+++
@@ -1,6 +0,0 @@
-Citizen.CreateThread(function()
-    if GetCurrentResourceName() ~= "force_lib" then
-        Funcs.DebugPrint("^1The resourcs need to be namned 'force_lib' otherwise the exports in my scripts won't work :(")
-        StopResource(GetCurrentResourceName())
-    end
-end)
